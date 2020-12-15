b14ab50278a42c28382ce4902773c67a12db4c28
ningrong <ningrong@minieye.cc>
2020-12-15 18:33:24 +0800

    hire: 我们只需要学习理解使用算法, 就能站在巨人的肩膀上

---
+++ WEB2020实习生招聘/t04-useAlgoOfCryptography.py
@@ -0,0 +1,22 @@
+# 你听过非对称加密吗?
+# 你了解过 RSA 吗?
+# 不了解也没关系, 我们只需要学习理解使用算法, 就能站在巨人的肩膀上.
+# 现在, 请你实现以下函数, 业务场景常见于ssh登录
+# 温馨提示: 你可以自己实现算法, 也可以使用标准库或第三方库
+
+def rsa_encode(message, public_key):
+    """
+    :param message: 明文消息
+    :param public_key: 公钥
+    :return: 密文消息
+    """
+    pass
+
+
+def rsa_decode(cipher_text, private_key):
+    """
+    :param cipher_text: 密文消息
+    :param private_key: 密钥
+    :return: 明文消息
+    """
+    pass
